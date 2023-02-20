2e1590029fb7a8810bb2fa56df738da8eb2e9f52
phamquanghien <pqh.cntt@gmail.com>
2023-02-20 15:09:32 +0700

    first commit

---
+++ BaiThucHanh2002/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BaiThucHanh2002.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
